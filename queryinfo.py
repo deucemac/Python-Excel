03201c2bebdef6591c69e5052cd4fdc48604b3dd
Dwayne McDowell <dwaynemcdowell@Dwaynes-MacBook-Pro.local>
2021-02-27 20:26:00 -0500

    population sum calculations

--- queryinfo.py
+++ queryinfo.py
@@ -6,6 +6,25 @@ print('The population of Brookly in 2010 was ' + str(brooklyn_pop) + ' .')
 fayette_county_pop = census2010.allData['GA']['Fayette']['pop']
 print('The population of Fayette County(my home county) in 2010 was ' + str(fayette_county_pop) + ' .')
 # Find population sum of a particular state
-for i in census2010.allData['GA']:
+
+
+
+
+# def total_state_pop():
+#   sum = 0
+#   for i in census2010.allData['GA']:
+#     sum = int(i['pop']) + sum
+#   return sum
+def total_state_pop(state):
   sum = 0
-  i['']
+  for i in census2010.allData[state]:
+    for x in census2010.allData[state][i]:
+      pop = census2010.allData[state][i]['pop']
+      sum = sum + pop
+  return sum
+
+
+new_york_pop = str(total_state_pop('NY'))
+north_dakota_pop = str(total_state_pop('ND'))
+print('The population of NY is ' + new_york_pop + ', but the population of North Dakota is only ' + north_dakota_pop)
+
